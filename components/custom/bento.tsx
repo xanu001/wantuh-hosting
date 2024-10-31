fbfa1c6ffe5def9e7bdfd53d13709ccc6fda04a1
xanu001 <thomas.barrett.22@sandon.essex.sch.uk>
2024-10-31 11:06:09 +0000

    first commit

---
+++ components/custom/bento.tsx
@@ -0,0 +1,131 @@
+"use client";
+import { clsx } from "clsx";
+import { motion } from "framer-motion";
+import Particles from "../ui/particles";
+import { BorderBeam } from "../ui/border-beam";
+import HyperText from "../ui/hyper-text";
+import RetroGrid from "../ui/retro-grid";
+
+export default function FUIBentoGridDark() {
+  return (
+    <div className="pt-32 container mx-auto">
+      <h1 className="font-geistMono tracking-tight text-3xl md:text-5xl">
+      <HyperText
+      className=""
+      text="about us"
+    />
+      </h1>
+      
+      <p className="max-w-3xl text-2xl/8 font-medium tracking-tight mt-2">
+        
+      </p>
+      <div className="mt-10 grid grid-cols-1 gap-4 sm:mt-16 lg:grid-cols-6 lg:grid-rows-2">
+        <BentoCard
+          eyebrow="Powerful servers"
+          title="Get perfect clarity"
+          description="PerkAI uses social engineering to build a detailed financial picture of your leads. Know their budget, compensation package, social security number, and more."
+          graphic={
+            <div className="absolute inset-0 bg-[url(https://framerusercontent.com/images/ghyfFEStl6BNusZl0ZQd5r7JpM.png)] object-fill" />
+          }
+          className="max-lg:rounded-t-4xl lg:col-span-3 lg:rounded-tl-4xl"
+        />
+        <BentoCard
+          eyebrow="Analysis"
+          title="Undercut your competitors"
+          description="With our advanced data mining, you’ll know which companies your leads are talking to and exactly how much they’re being charged."
+          graphic={
+            <div className="absolute inset-0 bg-[url(https://framerusercontent.com/images/7CJtT0Pu3w1vNADktNltoMFC9J4.png)] object-fill" />
+          }
+          className="lg:col-span-3 lg:rounded-tr-4xl"
+        />
+        <BentoCard
+          eyebrow="Speed"
+          title="Built for power users"
+          description="It’s never been faster to cold email your entire contact list using our streamlined keyboard shortcuts."
+          graphic={
+            <div className="absolute  inset-0 -top-20 -left-60 bg-[url(https://framerusercontent.com/images/gR21e8Wh6l3pU6CciDrqt8wjHM.png)] object-scale-down" />
+          }
+          className="lg:col-span-2 lg:rounded-bl-4xl"
+        />
+        <BentoCard
+          eyebrow="Source"
+          title="Get the furthest reach"
+          description="Bypass those inconvenient privacy laws to source leads from the most unexpected places."
+          graphic={
+            <div className="absolute inset-0 bg-[url(https://framerusercontent.com/images/PTO3RQ3S65zfZRFEGZGpiOom6aQ.png)] object-contain" />
+          }
+          className="lg:col-span-2"
+        />
+        <BentoCard
+        
+          eyebrow="Limitless"
+          title="Sell globally"
+          description="PerkAI helps you sell in locations currently under international embargo."
+          graphic={
+            <div className="absolute inset-0 -top-44 -left-60 bg-[url(https://framerusercontent.com/images/h496iPSwtSnGZwpJyErl6cLWdtE.png)] object-contain" />
+          }
+          className="max-lg:rounded-b-4xl lg:col-span-2 lg:rounded-br-4xl"
+        />
+      </div>
+      <Particles
+        className="absolute inset-0"
+        quantity={100}
+        ease={80}
+        refresh
+      />
+    </div>
+  );
+}
+export function BentoCard({
+  dark = false,
+  className = "",
+  eyebrow,
+  title,
+  description,
+  graphic,
+  fade = [],
+}: {
+  dark?: boolean;
+  className?: string;
+  eyebrow: React.ReactNode;
+  title: React.ReactNode;
+  description: React.ReactNode;
+  graphic?: React.ReactNode;
+  fade?: ("top" | "bottom")[];
+}) {
+  return (
+    
+    <motion.div
+      initial="idle"
+      whileHover="active"
+      variants={{ idle: {}, active: {} }}
+      data-dark={dark ? "true" : undefined}
+      className={clsx(
+        className,
+        "group relative flex flex-col overflow-hidden rounded-lg",
+        "bg-transparent transform-gpu dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#8686f01f_inset]   shadow-sm ring-1 ring-black/5",
+        "data-[dark]:bg-gray-800 data-[dark]:ring-white/15"
+      )}
+    >
+              <BorderBeam colorFrom="#A020F0" colorTo=""/>
+      <div className="relative h-[29rem] shrink-0">
+        {graphic}
+        {fade.includes("top") && (
+          <div className="absolute inset-0 bg-gradient-to-b from-white to-50% group-data-[dark]:from-gray-800 group-data-[dark]:from-[-25%] opacity-25" />
+        )}
+        {fade.includes("bottom") && (
+          <div className="absolute inset-0 bg-gradient-to-t from-white to-50% group-data-[dark]:from-gray-800 group-data-[dark]:from-[-25%] opacity-25" />
+        )}
+      </div>
+      <div className="relative p-10  z-20 isolate mt-[-110px] h-[14rem] backdrop-blur-xl">
+        <h1>{eyebrow}</h1>
+        <p className="mt-1 text-2xl/8 font-medium tracking-tight ">
+          {title}
+        </p>
+        <p className="mt-2 max-w-[600px] text-sm/6 text-gray-600 dark:text-gray-300 group-data-[dark]:text-gray-400">
+          {description}
+        </p>
+      </div>
+    </motion.div>
+  );
+}
---
+++ components/custom/hero.tsx
@@ -0,0 +1,88 @@
+
+import React from "react";
+import clsx from "clsx";
+import Particles from "../ui/particles";
+// import { MoveDown, SquareArrowOutUpRight } from "lucide_react"
+
+export default function FUIDarkHeroSection() {
+  const navigation = [
+    { title: "Customers", path: "javascript:void(0)" },
+    { title: "Careers", path: "javascript:void(0)" },
+  ];
+
+  return (
+    <div className="relative min-h-[60vh]">
+       <Particles
+        className="absolute inset-0"
+        quantity={100}
+        ease={80}
+        refresh
+      />
+      <Container className="relative  py-20 sm:pb-24 sm:pt-36">
+        <img
+          src="https://tailwindcss.com/_next/static/media/docs@30.8b9a76a2.avif"
+          className="absolute z-2 -top-0 left-10"
+        />
+
+        <div className="mx-auto max-w-2xl lg:max-w-4xl lg:px-12">
+          <h1 className="font-geist text-5xl font-normal  tracking-tighter  bg-gradient-to-r from-zinc-100 via-stone-200/50 to-purple-200/70 bg-clip-text  text-transparent sm:text-7xl">
+            <span className="sr-only">DeceptiConf - </span>The new
+            way to host bots.
+          </h1>
+          <div className="mt-6 space-y-6 font-geist text-md sm:text-xl tracking-tight text-gray-500">
+            <p>
+              We value your data & your time. So while you go with the best, we do too. We're up day and night keeping your servers well maintained and healthy.
+            </p>
+            <p>
+              At wantuh hosting, we provide top quality discord bot hosting for less than our competitors.
+            </p>
+          </div>
+          <div className="flex gap-6">
+          <button className=" gap-2 mt-4 w-full md:w-40 font-geist tracking-tighter text-center rounded-md text-md bg-gradient-to-br from-zinc-400 to-zinc-700 px-4 py-2 text-lg text-zinc-50 ring-2 ring-zinc-500/50 ring-offset-2 ring-offset-zinc-950 transition-all hover:scale-[1.02] hover:ring-transparent active:scale-[0.98] active:ring-zinc-500/70 flex items-center justify-center ">
+            Get Started
+            {/* <MoveDown size={16}/> */}
+          </button>
+          <button className=" gap-2 mt-4 w-full md:w-40 font-geist tracking-tighter text-center rounded-md text-md bg-gradient-to-br from-blue-400 to-blue-1200 px-4 py-2 text-lg text-zinc-50 ring-2 ring-zinc-500/50 ring-offset-2 ring-offset-zinc-950 transition-all hover:scale-[1.02] hover:ring-transparent active:scale-[0.98] active:ring-zinc-500/70 flex items-center justify-center ">
+            Contact us
+            {/* <SquareArrowOutUpRight size={16}/> */}
+          </button>
+          </div>
+        
+          <dl className="mt-10 grid grid-cols-2 gap-x-10 gap-y-6 sm:mt-16 sm:gap-x-16 sm:gap-y-10 sm:text-center lg:auto-cols-auto lg:grid-flow-col lg:grid-cols-none lg:justify-start lg:text-left">
+            {[
+              ["Providing for", "25+"],
+              ["Location", "United Kingdom"],
+            ].map(([name, value]) => (
+              <div key={name}>
+                <dt className="font-mono text-sm text-gray-300">{name}</dt>
+                <dd className="mt-0.5 text-2xl font-normal font-geist tracking-tight text-gray-300">
+                  {value}
+                </dd>
+              </div>
+            ))}
+          </dl>
+        </div>
+      </Container>
+    </div>
+  );
+}
+
+
+
+export function Container({
+  className,
+  ...props
+}: React.ComponentPropsWithoutRef<"div">) {
+  return (
+    <div
+      className={clsx("mx-auto max-w-7xl px-4 sm:px-6 lg:px-8", className)}
+      {...props}
+    />
+  );
+}
+
+
+  
+
+      
+      
